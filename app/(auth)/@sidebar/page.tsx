51203b704b6b91b3022218afe42c4f5b96419ac8
NatiGK <natnaelgashu2022@gmail.com>
2023-11-10 10:27:16 +0300

    fix sidebar

--- app/(auth)/@sidebar/page.tsx
+++ app/(auth)/@sidebar/page.tsx
@@ -73,7 +73,7 @@ export default function ChatSidebar(){
     const manipQuery = useManipQuery();
     const {selectedChat,selectedGroup,selectedUser} = useCustomParams()
     return(
-        <div className="h-full w-full max-w-full dark:bg-dark bg-light border-r-2 border-black/10 grid grid-cols-[80px,1fr]">
+        <div className="h-screen w-full max-w-full dark:bg-dark bg-light border-r-2 border-black/10 grid grid-cols-[80px,1fr]">
             <div className="bg-black/20">
                 <div className="h-[65px] w-full flex items-center justify-center px-1 mb-6">
                     <Image height={200} width={200} alt="icon" src={"/message-icon.png"} className="h-[35px] w-[35px]"/>
@@ -91,71 +91,73 @@ export default function ChatSidebar(){
                     text="Groups"
                 />
             </div>
-            <div>
+            <div className="h-screen grid grid-rows-[65px,1fr]">
                 <div className="h-[65px] w-full flex items-baseline justify-center px-4 mb-6">
                     <LineTextfield attr={{placeholder:"Search",value:query,onChange:(e)=>{setQuery(e.target.value)}}}/>
                 </div>
-                {
-                    tab!=="groups"&&(data?.data?.length as number>0)&&
-                        <p className="text-sm px-4 my-2 text-light-text-lighter dark:text-dark-text-darker bg-white/10 py-1">
-                            Users
-                        </p>
-                }
-                {tab!=="groups"&&data?.data.map((user)=>(
-                    <Link href={"/?"+manipQuery('user',user.id?.toString(),["group","chat"])} key={user.id}>
-                        <UserListItem
-                            firstName={user.firstName}
-                            lastName={user.lastName}
-                            selected={selectedUser===user.id}
-                            profileImg={user.profileImg}
-                        />
-                    </Link>
-                ))}
-                {
-                    tab==="groups"&&(groups?.data?.length as number>0)&&
-                        <p className="text-sm px-4 my-2 text-light-text-lighter dark:text-dark-text-darker bg-white/10 py-1">
-                            Groups
-                        </p>
-                }
-                {tab==="groups"&&groups?.data.map((group)=>(
-                    <Link href={"/?"+manipQuery('group',group.id?.toString(),["chat","user"])} key={group.id}>
-                        <UserListItem
-                            firstName={group.name}
-                            lastMessage={group.link}
-                            selected={selectedGroup===group.id}
-                            profileImg={group.profileImg}
-                        />
-                    </Link>
-                ))}
-                {
-                    (myChats?.length as number>0)&&
-                        <p className="text-sm px-4 my-2 text-light-text-lighter dark:text-dark-text-darker bg-white/10 py-1">
-                            {tab==="groups"?"Group":"User"} Chats
-                        </p>
-                }
-                {myChats?.map((chat)=>(
-                    <Link href={"/?"+manipQuery('chat',chat.id?.toString(),["group","user"])} key={chat.id}>
-                        <UserListItem
-                            firstName={chat.groupId===null?(me?.id===chat.user1.id?chat.user2.firstName:chat.user1.firstName):chat.group?.name}
-                            lastName={chat.groupId===null?(me?.id===chat.user1.id?chat.user2.lastName:chat.user1.lastName):""}
-                            selected={selectedChat===chat.id}
-                            lastMessage={chat.messages[0]?.text||""}
-                            lastMessageByYou={chat.messages[0]?.senderId===me?.id}
-                            lastSent={chat.messages[0]?.updatedAt?new Date(chat.messages[0]?.updatedAt):new Date(chat.messages[0]?.timeStamp)}
-                            profileImg={chat.groupId===null?(me?.id===chat.user1.id?chat.user2.profileImg:chat.user1.profileImg):undefined}
-                        />
-                    </Link>
-                ))}
-                {
-                    tab==="groups"&&(
-                        <div className="px-4">
-                            <Button attr={{onClick:onOpenCreateGroupModal}}>
-                                <BiPlus className="text-2xl"/>
-                                Create Group
-                            </Button>
-                        </div>
-                    )
-                }
+                <div className="h-full max-h-full overflow-auto">
+                    {
+                        tab!=="groups"&&(data?.data?.length as number>0)&&
+                            <p className="text-sm px-4 my-2 text-light-text-lighter dark:text-dark-text-darker bg-white/10 py-1">
+                                Users
+                            </p>
+                    }
+                    {tab!=="groups"&&data?.data.map((user)=>(
+                        <Link href={"/?"+manipQuery('user',user.id?.toString(),["group","chat"])} key={user.id}>
+                            <UserListItem
+                                firstName={user.firstName}
+                                lastName={user.lastName}
+                                selected={selectedUser===user.id}
+                                profileImg={user.profileImg}
+                            />
+                        </Link>
+                    ))}
+                    {
+                        tab==="groups"&&(groups?.data?.length as number>0)&&
+                            <p className="text-sm px-4 my-2 text-light-text-lighter dark:text-dark-text-darker bg-white/10 py-1">
+                                Groups
+                            </p>
+                    }
+                    {tab==="groups"&&groups?.data.map((group)=>(
+                        <Link href={"/?"+manipQuery('group',group.id?.toString(),["chat","user"])} key={group.id}>
+                            <UserListItem
+                                firstName={group.name}
+                                lastMessage={group.link}
+                                selected={selectedGroup===group.id}
+                                profileImg={group.profileImg}
+                            />
+                        </Link>
+                    ))}
+                    {
+                        (myChats?.length as number>0)&&
+                            <p className="text-sm px-4 my-2 text-light-text-lighter dark:text-dark-text-darker bg-white/10 py-1">
+                                {tab==="groups"?"Group":"User"} Chats
+                            </p>
+                    }
+                    {myChats?.map((chat)=>(
+                        <Link href={"/?"+manipQuery('chat',chat.id?.toString(),["group","user"])} key={chat.id}>
+                            <UserListItem
+                                firstName={chat.groupId===null?(me?.id===chat.user1.id?chat.user2.firstName:chat.user1.firstName):chat.group?.name}
+                                lastName={chat.groupId===null?(me?.id===chat.user1.id?chat.user2.lastName:chat.user1.lastName):""}
+                                selected={selectedChat===chat.id}
+                                lastMessage={chat.messages[0]?.text||""}
+                                lastMessageByYou={chat.messages[0]?.senderId===me?.id}
+                                lastSent={chat.messages[0]?.updatedAt?new Date(chat.messages[0]?.updatedAt):new Date(chat.messages[0]?.timeStamp)}
+                                profileImg={chat.groupId===null?(me?.id===chat.user1.id?chat.user2.profileImg:chat.user1.profileImg):undefined}
+                            />
+                        </Link>
+                    ))}
+                    {
+                        tab==="groups"&&(
+                            <div className="px-4">
+                                <Button attr={{onClick:onOpenCreateGroupModal}}>
+                                    <BiPlus className="text-2xl"/>
+                                    Create Group
+                                </Button>
+                            </div>
+                        )
+                    }
+                </div>
                 {showModal&&<CreateGroupModal onClose={onCloseCreateGroupModal}/>}
             </div>
         </div>
